ffdd5409a4d8ac77cde28c78af08c2a621fe5351
Jair Pérez <oquendojair10@gmail.com>
2023-01-25 15:17:38 -0500

    update domain in layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout(props) {
   const meta = {
     title: "Jair Pérez",
     description: `Portfolio Jair Pérez`,
-    image: "#",
+    image: "/photo.jpg",
     type: "website",
     ...customMeta,
   };
@@ -17,15 +17,21 @@ export default function Layout(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://domain${router.asPath}`} />
-        <link rel="canonical" href={`https://domain${router.asPath}`} />
+        <meta
+          property="og:url"
+          content={`https://oquendo.vercel.app${router.asPath}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://oquendo.vercel.app${router.asPath}`}
+        />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Prject P" />
+        <meta property="og:site_name" content="Jair Pérez" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@project-p" />
+        <meta name="twitter:site" content="@perezjair94" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
