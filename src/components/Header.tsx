3f09ca40d83bc1bd6ee46e2a8560193b880a8855
Jair Pérez <oquendojair10@gmail.com>
2023-09-05 13:18:49 -0500

    feat(Header): change icon size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,11 +12,17 @@ export default function Header() {
           <SocialLink
             link="https://instagram.com/perezjair94"
             icon="Instagram"
+            size={24}
           />
-          <SocialLink link="https://github.com/perezjair94" icon="Github" />
+          <SocialLink
+            link="https://github.com/perezjair94"
+            icon="Github"
+            size={24}
+          />
           <SocialLink
             link="https://www.linkedin.com/in/perezjair94/"
             icon="Linkedin"
+            size={24}
           />
         </div>
       </Content>
