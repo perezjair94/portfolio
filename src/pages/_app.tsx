aeacbcd812060a67d8dc8dfc47ba4c52c1de09be
Jair Pérez <oquendojair10@gmail.com>
2023-09-05 10:30:04 -0500

    feat(_app): remove cache provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { cache } from "@emotion/css";
-import { CacheProvider } from "@emotion/react";
 import GlobalStyles from "@/utlis/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={cache}>
+    <div>
       <GlobalStyles />
       <Component {...pageProps} />
-    </CacheProvider>
+    </div>
   );
 }
